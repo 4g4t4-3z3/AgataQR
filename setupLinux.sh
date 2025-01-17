aafa8f7ec1428c617063b8fc608f706cd7fd2e6a
4g4t4-3z3 <randomssh@duck.com>
2025-01-17 15:20:08 -0300

    Update setupLinux.sh

--- setupLinux.sh
+++ setupLinux.sh
@@ -4,16 +4,16 @@
 NOMBRE_PROGRAMA="QRGenerate"
 RUTA_ICONO="$PWD/qrgo.ico"
 RUTA_SCRIPT="$PWD/QRGenerate.py"
-RUTA_DESTINO="/opt/AgataQR"
+RUTA_DESTINO="$HOME/AgataQR"  # Ahora guardamos en el directorio del usuario
 RUTA_DESKTOP="$HOME/.local/share/applications/QRGenerate.desktop"
 
 # Crear carpeta de destino
 echo "Creando carpeta de instalación..."
-sudo mkdir -p $RUTA_DESTINO
+mkdir -p $RUTA_DESTINO
 
 # Copiar archivos al destino
 echo "Copiando archivos..."
-sudo cp "$RUTA_SCRIPT" "$RUTA_ICONO" $RUTA_DESTINO
+cp "$RUTA_SCRIPT" "$RUTA_ICONO" $RUTA_DESTINO
 
 # Crear archivo .desktop
 echo "Creando acceso directo..."
@@ -22,13 +22,13 @@ cat << EOF > $RUTA_DESKTOP
 Version=1.0
 Type=Application
 Name=$NOMBRE_PROGRAMA
-Exec=/usr/bin/python3 $RUTA_DESTINO/QRGenerate.py
+Exec=python3 $RUTA_DESTINO/QRGenerate.py
 Icon=$RUTA_DESTINO/qrgo.ico
 Terminal=false
 EOF
 
 # Dar permisos de ejecución
-sudo chmod +x $RUTA_SCRIPT
+chmod +x $RUTA_SCRIPT
 chmod +x $RUTA_DESKTOP
 
 # Actualizar caché de aplicaciones
